266011f5c83c26d5e263120afeda0d919f386b7e
Quentin Gruber <quentingruber@gmail.com>
2021-10-03 21:27:05 +0200

    prettier + fix

--- main.ts
+++ main.ts
@@ -1,135 +1,154 @@
-import { ChildProcess, spawn } from 'child_process';
-import { Command } from 'commander';
-import { funcObj } from './funcObj';
+import { ChildProcess, spawn } from "child_process";
+import { Command } from "commander";
+import { funcObj } from "./funcObj";
 import * as fs from "fs";
 
 const program = new Command();
 program
-  .option('-p, --path <type>', 'Path to the script')
-  .option('-o, --output <type>', 'Path to the output file')
-  .option('-d , --debug', 'Debug mode')
-  .option('-do , --deopt-only', 'Render deopt only')
-  .option('-oo , --opt-only', 'Render opt only')
-  .option('-e, --exec-json', 'Render in a html a previously done json output')
-  .option('-t, --time <type>', 'To choose how many time before the program shutdown')
-  program.parse(process.argv);
-  console.log(program.opts())
-if(program.opts().execJson){
-  throw "unimplemented"
+  .option("-p, --path <type>", "Path to the script")
+  .option("-o, --output <type>", "Path to the output file")
+  .option("-d , --debug", "Debug mode")
+  .option("-do , --deopt-only", "Render deopt only")
+  .option("-oo , --opt-only", "Render opt only")
+  .option("-e, --exec-json", "Render in a html a previously done json output")
+  .option(
+    "-t, --time <type>",
+    "To choose how many time before the program shutdown"
+  );
+program.parse(process.argv);
+console.log(program.opts());
+if (program.opts().execJson) {
+  throw "unimplemented";
+} else {
+  execProcess(program.opts().path, program.opts().time);
 }
-else{
-  execProcess(program.opts().path,program.opts().time)
-}
 
-
-
-function execProcess(path:string,time?:number){
+function execProcess(path: string, time?: number) {
   let processId;
-  if(time){
-    processId = setTimeout(()=>endProcess(ls,stringArray),time * 1000);
+  if (time) {
+    processId = setTimeout(() => endProcess(ls, stringArray), time * 1000);
   }
-  console.log("executing :"+path)
+  console.log("executing :" + path);
   const spawnArgs = [];
-  if(!program.opts().optOnly){
-    spawnArgs.push("--trace-deopt")
+  if (!program.opts().optOnly) {
+    spawnArgs.push("--trace-deopt");
   }
-  if(!program.opts().deoptOnly){
-    spawnArgs.push("--trace-opt")
+  if (!program.opts().deoptOnly) {
+    spawnArgs.push("--trace-opt");
   }
-  spawnArgs.push(path)
-  const ls:ChildProcess = spawn('node', spawnArgs);
-  const stringArray:string[] = []
-  
-  ls.stdout.on('data', (data:Buffer) => {
-    stringArray.push(data.toString())
+  spawnArgs.push(path);
+  const ls: ChildProcess = spawn("node", spawnArgs);
+  const stringArray: string[] = [];
+
+  ls.stdout.on("data", (data: Buffer) => {
+    stringArray.push(data.toString());
   });
-  
-  ls.on('close', (code) => {
+
+  ls.on("close", (code) => {
     clearTimeout(processId);
     console.log(`child process exited with code ${code}`);
-    if(code)
-      endProcess(ls,stringArray);
+    if (code) endProcess(ls, stringArray);
   });
-  process.on('SIGINT', function() {
+  process.on("SIGINT", function () {
     console.log("Caught interrupt signal on main");
-    endProcess(ls,stringArray)
+    endProcess(ls, stringArray);
   });
 }
 
-function endProcess(ls:ChildProcess,stringArray:string[]){
-  ls.kill('SIGINT')
+function endProcess(ls: ChildProcess, stringArray: string[]) {
+  ls.kill("SIGINT");
   //console.log(stringArray)
   parseStringToObjectJs(stringArray);
-} 
+}
 
-function parseStringToObjectJs(stringArray:string[]){
-  const funcCollection:any = {};
-    for (let index = 0; index < stringArray.length; index++) {
-      const line = stringArray[index];
-      if(line[0] == "["){
-        const v8Lines = line.split("\r\n")
-        for (let index = 0; index < v8Lines.length - 1; index++) {
-          const v8Line = v8Lines[index];
-          const deoptimizing = v8Line.includes("deoptimization") || v8Line.includes("deoptimizing");
-          const currentFunctionId = v8Line.split("(sfi = ")[1]?.split(")")[0];
-          const operation = v8Line.split(" ")[0].substring(1);
-          let currentFunction: funcObj;
-          if(!funcCollection[currentFunctionId]){
-            currentFunction = new funcObj(currentFunctionId);
-            funcCollection[currentFunctionId] = currentFunction
+function parseStringToObjectJs(stringArray: string[]) {
+  const funcCollection: any = {};
+  for (let index = 0; index < stringArray.length; index++) {
+    const line = stringArray[index];
+    console.log(line + "\n\n\n\n");
+    const v8Lines = line.split("\r\n");
+    for (let index = 0; index < v8Lines.length - 1; index++) {
+      const v8Line = v8Lines[index];
+      const deoptimizing =
+        v8Line.includes("deoptimization") || v8Line.includes("deoptimizing");
+      const currentFunctionId = v8Line.split("(sfi = ")[1]?.split(")")[0];
+      const operation = v8Line.split(" ")[0].substring(1);
+      let currentFunction: funcObj;
+      if (!funcCollection[currentFunctionId]) {
+        currentFunction = new funcObj(currentFunctionId);
+        funcCollection[currentFunctionId] = currentFunction;
+      }
+      if (program.opts().debug) {
+        funcCollection[currentFunctionId].debugLine = v8Line;
+      }
+      funcCollection[currentFunctionId].executionOrder.push(operation);
+      switch (operation) {
+        case "marking": {
+          if (deoptimizing) {
+            continue;
+            const currentSpecialFunctionId = v8Line
+              .split("(")[1]
+              ?.split(" ")[0];
+            funcCollection[currentSpecialFunctionId].markedReason = v8Line
+              .split("reason: ")[1]
+              .split("]")[0];
+            funcCollection[currentSpecialFunctionId].name = v8Line
+              .split("<SharedFunctionInfo ")[1]
+              .split(" >")[0];
+          } else {
+            funcCollection[currentFunctionId].markedReason = v8Line
+              .split("reason: ")[1]
+              .split("]")[0];
+            funcCollection[currentFunctionId].name = v8Line
+              .split("<JSFunction ")[1]
+              .split(" (")[0];
           }
-          if(program.opts().debug){
-            funcCollection[currentFunctionId].debugLine = v8Line
-          }
-          funcCollection[currentFunctionId].executionOrder.push(operation)
-          switch (operation) {
-            case "marking":{
-              if(deoptimizing){
-                continue
-                const currentSpecialFunctionId = v8Line.split("(")[1]?.split(" ")[0];
-                funcCollection[currentSpecialFunctionId].markedReason = v8Line.split("reason: ")[1].split("]")[0];
-                funcCollection[currentSpecialFunctionId].name = v8Line.split("<SharedFunctionInfo ")[1].split(" >")[0];
-              }
-              else{
-                funcCollection[currentFunctionId].markedReason = v8Line.split("reason: ")[1].split("]")[0];
-                funcCollection[currentFunctionId].name = v8Line.split("<JSFunction ")[1].split(" (")[0];
-              }
-              break;
-            }
-            case "compiling":{
-              funcCollection[currentFunctionId].compilationMethod = v8Line.split(")> ")[1].split("]")[0];
-              break;
-            }
-            case "optimizing":{
-              funcCollection[currentFunctionId].optimizationMethod = v8Line.split(")> ")[1].split("]")[0];
-              break;
-            }
-            case "completed":{
-              funcCollection[currentFunctionId].isOptimized = true;
-              break;
-            }
-            case "bailout":{
-              console.log("bailout");
-              funcCollection[currentFunctionId].isDeoptimized = true;
-              funcCollection[currentFunctionId].name = v8Line.split("<JSFunction ")[1].split(" (")[0];
-              funcCollection[currentFunctionId].name =  funcCollection[currentFunctionId].name.length > 30 ? "Anonymous": funcCollection[currentFunctionId].name
-              funcCollection[currentFunctionId].deoptimizationReason = v8Line.split("reason: ")[1].split(")")[0];
-              funcCollection[currentFunctionId].deoptimizationKind = v8Line.split("(kind: ")[1].split(",")[0];
-              break;
-            }
-            default:
-              break;
-          }
+          break;
         }
+        case "compiling": {
+          funcCollection[currentFunctionId].compilationMethod = v8Line
+            .split(")> ")[1]
+            .split("]")[0];
+          break;
+        }
+        case "optimizing": {
+          funcCollection[currentFunctionId].optimizationMethod = v8Line
+            .split(")> ")[1]
+            .split("]")[0];
+          break;
+        }
+        case "completed": {
+          funcCollection[currentFunctionId].isOptimized = true;
+          break;
+        }
+        case "bailout": {
+          funcCollection[currentFunctionId].isDeoptimized = true;
+          funcCollection[currentFunctionId].name = v8Line
+            .split("<JSFunction ")[1]
+            .split(" (")[0];
+          funcCollection[currentFunctionId].name =
+            funcCollection[currentFunctionId].name.length > 30
+              ? "Anonymous"
+              : funcCollection[currentFunctionId].name;
+          funcCollection[currentFunctionId].deoptimizationReason = v8Line
+            .split("reason: ")[1]
+            .split(")")[0];
+          funcCollection[currentFunctionId].deoptimizationKind = v8Line
+            .split("(kind: ")[1]
+            .split(",")[0];
+          break;
+        }
+        default:
+          break;
       }
     }
-    console.log(funcCollection)
-    const outputPath = program.opts().output
-    const funcCollectionJson = JSON.stringify(funcCollection);
-    if(outputPath){
-      fs.writeFileSync(`${__dirname}/${outputPath}`,funcCollectionJson);
-    }
-    else{
-      fs.writeFileSync(`${__dirname}/output.json`,funcCollectionJson);
-    }
+  }
+  console.log(funcCollection);
+  const outputPath = program.opts().output;
+  const funcCollectionJson = JSON.stringify(Object.values(funcCollection));
+  if (outputPath) {
+    fs.writeFileSync(`${__dirname}/${outputPath}`, funcCollectionJson);
+  } else {
+    fs.writeFileSync(`${__dirname}/output.json`, funcCollectionJson);
+  }
 }
